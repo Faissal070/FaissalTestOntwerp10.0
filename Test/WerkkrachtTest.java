6526cf95438b2710809ed9b7516f7c8d3fc976d6
Faissal070 <faisszal@hotmail.com>
2021-06-04 00:50:12 +0200

    Pushen naar master

--- Test/WerkkrachtTest.java
+++ Test/WerkkrachtTest.java
@@ -5,21 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class WerkkrachtTest {
 
-    @Test
-    void checkPerson() {
-        Werkkracht persoon = new Werkkracht("S",9876543,8765);
-
-        String naam = "Faissal";
-        int code = 43233;
-        int bsn = 98763235;
-        Assertions.assertEquals(false, persoon.checknaamArray()); // naam
-        // is niet opgeslagen dus false klopt
-        Assertions.assertEquals(false, persoon.checkCodeRegel()); // false want code bevat meer dan 4 cijfers
-        Assertions.assertEquals(false, persoon.checkbsn()); // false want bsn < 9
-        //Nog geen code in de methode
-        //TDD
-
-    }
 
     @Test
     void ModifiedDecisionCoverage() {
