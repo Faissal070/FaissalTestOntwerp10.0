d2df300260f252dbb5bfbf1601c6c9d95158bf40
Faissal070 <faisszal@hotmail.com>
2021-06-03 03:26:36 +0200

    MD/DC afgerond

--- src/Werkkracht.java
+++ src/Werkkracht.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+
 public class Werkkracht {
     private String naam;
     private int code;
     private int bsn;
+    public static ArrayList<Werkkracht> personen = new ArrayList<>();
+
+    Werkkracht (String naam, Integer bsn, Integer code){
+        this.naam = naam;
+        this.bsn = bsn;
+        this.code = code;
+    }
 
 
 
@@ -17,13 +26,26 @@ public class Werkkracht {
     public Integer getBsn() {
         return this.bsn;
     }
-    public boolean checknaamArray(String naam){
-        return false;
+    public String checknaamArray(){
+        if(personen.contains(this)) {
+            return "Je staat in ons systeem";
+        }
+        return "Sorry je staat niet in ons systeem";
+
     }
-    public boolean checkbsn(int bsn){
-        return false;
+    public String checkbsn() {
+        int numDigits = String.valueOf(bsn).length();
+        if (numDigits == 9) {
+            return "Je bsn klopt geweldig!";
+        }
+        return "Sorry je bsn nummer klopt niet";
     }
-    public boolean checkCodeRegel(int code){
-        return false;
+    public String checkCodeRegel(){
+        int numDigits = String.valueOf(code).length();
+        if(numDigits == 4){
+            return "Je code klopt";
+        }
+        return "Je code klopt niet";
+
     }
 }
